cb5f706cefd53ac70cb6697f143be65c7c0a6754
Ryker <rykguy17@gmail.com>
2025-11-26 17:32:07 -0600

    Following jose type requirements

--- lib/jwt.ts
+++ lib/jwt.ts
@@ -11,7 +11,7 @@ export interface JWTPayload {
 }
 
 // sign token with payload
-export async function signToken(payload: JWTPayload): Promise<string> {
+export async function signToken(payload: Record<string, unknown> & JWTPayload): Promise<string> {
   const token = await new SignJWT(payload)
     .setProtectedHeader({ alg: 'HS256' })
     .setIssuedAt()
