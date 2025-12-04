6e00ffe48c23dbdc5921c389cabc763bb5780be9
Aleksandr Khindanov <aleksandr.khindanov@trojans.dsu.edu>
2025-12-03 19:52:51 -0600

    Fixed UI

--- components/ProfilePanel.tsx
+++ components/ProfilePanel.tsx
@@ -8,21 +8,22 @@ export default function ProfilePanel() {
   const uploadSong = () => {
     alert("this will open a pop up window(maybe) to upload a song");
   };
+  const viewScores = () => {
+    alert("this will open a pop up window(maybe) to view your scores");
+  };
 
   return (
     <div style={{ padding: 12, background: '#fff', color: '#111', borderRadius: 6, boxShadow: '0 6px 18px rgba(0,0,0,0.15)', minWidth: 300 }}>
-      <h3 style={{ marginTop: 0 }}>My Profile</h3>
       
       {loading ? (
         <p>Loading...</p>
       ) : user ? (
         <div style={{ display: 'flex', flexDirection: 'column', gap: 12 }}>
           <div>
-            <div style={{ fontSize: 12, opacity: 0.8, marginBottom: 4 }}>Username</div>
-            <div style={{ fontSize: 16, fontWeight: 600 }}>{user.username}</div>
+            <div style={{fontWeight: 700, textAlign: 'center', fontSize: 20, paddingTop: 10}}>Hello, {user.username}!</div>
           </div>
           <button 
-            onClick={uploadSong}
+            onClick={viewScores}
             style={{ 
               marginTop: 8, 
               padding: '8px 16px', 
